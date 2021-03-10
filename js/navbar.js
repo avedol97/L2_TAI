d9a37f8ff3e606d752c95832137359c5c57c04c0
krystian <krystian.cwioro@o2.pl>
2021-03-10 20:05:19 +0100

    lab3 12.03.2021

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><a href="index.html">Home</a>' + ' ' + '<a href="contact.html">Contact</a><nav>';
+
+let navbarElem = document.querySelector(".navbar")
+
+navbarElem.innerHTML = nav;
